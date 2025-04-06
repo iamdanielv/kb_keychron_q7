7397b49c6cdb8459892fd70bb77140a7b8d5231b
Daniel <1767914+iamdanielv@users.noreply.github.com>
2025-04-05 21:03:43 -0700

    BUG: numpad 0 was off by one key

--- keychron/q7/ansi/keymaps/iamdanielv/keymap.c
+++ keychron/q7/ansi/keymaps/iamdanielv/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, KC_NUM,  KC_P7,   KC_P8,   KC_P9,    KC_PAST,  _______, _______,  _______,           _______, _______,
         _______, KC_BTN1,  KC_MS_U,  KC_BTN2, MSW_UP,  XXXXXXX, XXXXXXX, KC_P4,   KC_P5,   KC_P6,    KC_PPLS,  _______, _______,  _______,           _______, _______,
         _______, KC_MS_L,  KC_MS_D,  KC_MS_R, MSW_DN,  XXXXXXX, XXXXXXX, KC_P1,   KC_P2,   KC_P3,    KC_PENT,  _______,           _______,           _______, _______,
-        LSFT_LLCK,         XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P0,   KC_PDOT, KC_PDOT,  KC_PSLS,  _______,           _______,           _______, _______,
+        LSFT_LLCK,         XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P0,   KC_PDOT,  KC_PDOT,  KC_PSLS,           _______,           _______, _______,
         _______, _______,  _______,                             _______,                             _______,  TG_NUM,  _______,  _______,  _______, _______, _______),
 
     [MEDIA_LYR] = LAYOUT_ansi_72(
